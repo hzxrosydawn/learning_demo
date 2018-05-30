361b63795da30b3f58c9815e8377c2b2f8bebea8
Vincent Huang <hzxrosydawn@gmail.com>
2018-05-30 17:35:19 +0800

    modified the spring-boot demo module

--- spring-boot-demo/src/main/java/com/rosydawn/Application.java
+++ spring-boot-demo/src/main/java/com/rosydawn/Application.java
@@ -2,36 +2,40 @@ package com.rosydawn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-//import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.WebApplicationInitializer;
 
 import javax.sql.DataSource;
 
+/**
+ * 只有在构建和部署war包时才需要SpringBootServletInitializer。如果使用内置的容器，则不需要显式使用该类。
+ */
 //@Configuration
 //@ComponentScan
 //@EnableAutoConfiguration
 @SpringBootApplication // 代替上面三个注解
 @EnableTransactionManagement
-public class Application  /*extends SpringBootServletInitializer implements WebApplicationInitializer*/ {
+public class Application  extends SpringBootServletInitializer {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
 
+    /**
+     * 加入事务管理器
+     * @param dataSource 数据源
+     * @return 事务管理器
+     */
     @Bean
     public PlatformTransactionManager txManager(DataSource dataSource) {
         return new DataSourceTransactionManager(dataSource);
     }
 
-
+    // 重写SpringBootServletInitializer的方法
+    @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
     }
-
-
-
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
-    }
 }
-
